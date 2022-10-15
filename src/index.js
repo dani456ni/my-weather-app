3cf1b92dadbb6a59a389bf1b66ed82cfda2cf15c
dani456ni <114633755+dani456ni@users.noreply.github.com>
2022-10-14 22:26:55 -0500

    Fixed undefined

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return day[days];
+  return days[day];
 }
 
 function displayForecast(response) {
